44d355af0be0d2c5c6951012594581f8b7bffdaa
Vikranth Kanumuru <20688709+highskillzz@users.noreply.github.com>
2023-05-03 18:02:16 -0400

    initial setup

---
+++ src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Homepage</h1>
+      <p>This is the homepage of our React application.</p>
+    </div>
+  );
+};
+
+export default Homepage;
